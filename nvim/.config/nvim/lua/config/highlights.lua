5533ceebda168506d0f33c3a6c8f654a3a7ad5de
Magnus Gronas <magnus.s.gronas@gmail.com>
2025-02-15 20:53:15 +0100

    lualine modifications

--- nvim/.config/nvim/lua/config/highlights.lua
+++ nvim/.config/nvim/lua/config/highlights.lua
@@ -9,10 +9,14 @@ local hl = vim.api.nvim_set_hl
 -- vim.api.nvim_set_hl(0, "SnacksDashboardDesc", { fg = "#b4befe" })
 -- vim.api.nvim_set_hl(0, "SnacksDashboardKey", { fg = "#45475a" })
 -- vim.api.nvim_set_hl(0, "SnacksDashboardSpecial", { fg = "#f2cdcd" })
+
+-- Snacks Picker
 hl(0, "SnacksPickerBorder", { fg = "#181825", bg = "#181825" })
 hl(0, "SnacksPickerInputTitle", { fg = "#4c4f69", bg = "#cba6f7" })
 hl(0, "SnacksPickerInputBorder", { bg = "#11111b", fg = "#11111b" })
 hl(0, "SnacksPickerInput", { bg = "#11111b", fg = "#cdd6f4" })
 hl(0, "SnacksPickerListTitle", { bg = "#f2cdcd", fg = "#4c4f69" })
 hl(0, "SnacksPickerPreviewTitle", { bg = "#a6e3a1", fg = "#4c4f69" })
+
+-- Lualine
 hl(0, "LualineDiff", { fg = "#6e738d" })
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,15 +3,68 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VeryLazy",
 	config = function()
+		local lazy = require("lazy.status")
+
 		local hide = function()
 			return vim.fn.winwidth(0) > 100
 		end
+
+		-- Show currently attached lsp
+		local lsp = function()
+			local clients = vim.lsp.get_clients({ bufnr = 0 })
+			if #clients == 0 then
+				return ""
+			end
+
+			local client_names = {}
+			for _, client in ipairs(clients) do
+				table.insert(client_names, client.name)
+			end
+
+			if client_names[1] == "null-ls" then
+				return client_names[2]
+			end
+
+			return client_names[1]
+		end
+
+		-- Color overrides
+		local catppuccin = require("lualine.themes.catppuccin")
+
+		-- Custom colors
+		catppuccin.insert.a.fg = "#A6E3A1"
+		catppuccin.insert.a.bg = "#1E1E2E"
+		catppuccin.insert.a.gui = ""
+
+		catppuccin.normal.a.fg = "#89B4FA"
+		catppuccin.normal.a.bg = "#1E1E2E"
+		catppuccin.normal.a.gui = ""
+
+		catppuccin.visual.a.fg = "#A6E3A1"
+		catppuccin.visual.a.bg = "#1E1E2E"
+		catppuccin.visual.a.gui = ""
+
+		catppuccin.command.a.fg = "#FAB387"
+		catppuccin.command.a.bg = "#1E1E2E"
+		catppuccin.command.a.gui = ""
+
+		catppuccin.terminal.a.fg = "#F38BA8"
+		catppuccin.terminal.a.bg = "#1E1E2E"
+		catppuccin.terminal.a.gui = ""
+
+		catppuccin.replace.a.fg = "#F38BA8"
+		catppuccin.replace.a.bg = "#1E1E2E"
+		catppuccin.replace.a.gui = ""
 
 		require("lualine").setup({
 
 			options = {
 				disabled_filetypes = { "snacks_dashboard" },
+				section_separators = "",
+				component_separators = "",
+				theme = catppuccin,
 			},
+
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = {
@@ -30,7 +83,7 @@ return {
 					{
 						"branch",
 						icon = "󰘬",
-						color = { fg = "#6e738d" },
+						color = { fg = "#6e738d", bg = "#1E1E2E" },
 						cond = hide,
 					},
 					{
@@ -45,8 +98,37 @@ return {
 					},
 				},
 				lualine_x = {
-					"diagnostics",
+					{
+						"diagnostics",
+						update_in_insert = true,
+						cond = hide,
+					},
+					{
+						lazy.updates(),
+						cond = lazy.has_updates() or hide(),
+						color = { fg = "#fab387", bg = "#1E1E2E" },
+					},
+					{
+						lsp,
+						color = { fg = "#6e738d", bg = "#1E1E2E" },
+					},
 				},
+				lualine_y = {
+					{
+						"filetype",
+						icon_only = true,
+					},
+				},
+				lualine_z = {
+					"progress",
+					"location",
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "progress", "location" },
 				lualine_y = {},
 				lualine_z = {},
 			},
