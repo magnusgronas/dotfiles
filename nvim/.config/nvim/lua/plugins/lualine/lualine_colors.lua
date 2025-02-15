e1161cbf40416c160458f2bf4dd2e408a6c2cf9c
Magnus Gronas <magnus.s.gronas@gmail.com>
2025-02-15 21:25:49 +0100

    lualine

--- nvim/.config/nvim/lua/plugins/lualine/lualine_colors.lua
+++ nvim/.config/nvim/lua/plugins/lualine/lualine_colors.lua
@@ -3,6 +3,8 @@ local M = {}
 M.catppuccin = require("lualine.themes.catppuccin")
 
 -- Custom colors
+
+-- section a
 M.catppuccin.insert.a.fg = "#A6E3A1"
 M.catppuccin.insert.a.bg = "#1E1E2E"
 M.catppuccin.insert.a.gui = ""
@@ -11,7 +13,7 @@ M.catppuccin.normal.a.fg = "#89B4FA"
 M.catppuccin.normal.a.bg = "#1E1E2E"
 M.catppuccin.normal.a.gui = ""
 
-M.catppuccin.visual.a.fg = "#A6E3A1"
+M.catppuccin.visual.a.fg = "#cba6f7"
 M.catppuccin.visual.a.bg = "#1E1E2E"
 M.catppuccin.visual.a.gui = ""
 
@@ -27,7 +29,54 @@ M.catppuccin.replace.a.fg = "#F38BA8"
 M.catppuccin.replace.a.bg = "#1E1E2E"
 M.catppuccin.replace.a.gui = ""
 
-M.catppuccin.normal.b.fg = "#6e738d"
+-- section b
+M.catppuccin.normal.b.fg = "#cdd6f4"
 M.catppuccin.normal.b.bg = "#1E1E2E"
+M.catppuccin.normal.b.gui = ""
+
+M.catppuccin.insert.b.fg = "#cdd6f4"
+M.catppuccin.insert.b.bg = "#1E1E2E"
+M.catppuccin.insert.b.gui = ""
+
+M.catppuccin.visual.b.fg = "#cdd6f4"
+M.catppuccin.visual.b.bg = "#1E1E2E"
+M.catppuccin.visual.b.gui = ""
+
+M.catppuccin.command.b.fg = "#cdd6f4"
+M.catppuccin.command.b.bg = "#1E1E2E"
+M.catppuccin.command.b.gui = ""
+
+M.catppuccin.terminal.b.fg = "#cdd6f4"
+M.catppuccin.terminal.b.bg = "#1E1E2E"
+M.catppuccin.terminal.b.gui = ""
+
+M.catppuccin.replace.b.fg = "#cdd6f4"
+M.catppuccin.replace.b.bg = "#1E1E2E"
+M.catppuccin.replace.b.gui = ""
+
+-- section c
+M.catppuccin.normal.c.fg = "#cdd6f4"
+M.catppuccin.normal.c.bg = "#1E1E2E"
+M.catppuccin.normal.c.gui = ""
+
+M.catppuccin.insert.c.fg = "#cdd6f4"
+M.catppuccin.insert.c.bg = "#1E1E2E"
+M.catppuccin.insert.c.gui = ""
+
+M.catppuccin.visual.c.fg = "#cdd6f4"
+M.catppuccin.visual.c.bg = "#1E1E2E"
+M.catppuccin.visual.c.gui = ""
+
+M.catppuccin.command.c.fg = "#cdd6f4"
+M.catppuccin.command.c.bg = "#1E1E2E"
+M.catppuccin.command.c.gui = ""
+
+M.catppuccin.terminal.c.fg = "#cdd6f4"
+M.catppuccin.terminal.c.bg = "#1E1E2E"
+M.catppuccin.terminal.c.gui = ""
+
+M.catppuccin.replace.c.fg = "#cdd6f4"
+M.catppuccin.replace.c.bg = "#1E1E2E"
+M.catppuccin.replace.c.gui = ""
 
 return M
